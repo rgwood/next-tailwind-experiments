b01a839d3b1c86bd14011fc874e66b5713370600
Reilly <reilly.wood@icloud.com>
2019-03-07 00:35:10 +0900

    error at bottom

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,21 +8,24 @@ function searchInputMirror(input: string) {
   return <div className="text-purple">input</div>
 }
 
-export default withRouter((props) => { 
-  if(!props.router) {
-    throw new Error("Router not defined. Something's up with Next.js");
-  }
-  return <Layout title="Home Page">
+export default withRouter((props) => {
+  if (!!props.router) {
+    return <Layout title="Home Page">
       <p>Made with Tailwind CSS and Next.js</p>
       {/* <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
-      value={searchInput} onChange={e => setSearchInput(e.target.value)}/> */}
-      <Search router={props.router}/>
-       <div/>
+    value={searchInput} onChange={e => setSearchInput(e.target.value)}/> */}
+      <Search router={props.router} />
+      <div />
       {/* <button className="bg-white hover:bg-grey-lightest text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow" 
-      onClick={() => props.router ? props.router.push("/about") : console.log("router not defined") }>Click Me</button>
-      <div/> */}
+    onClick={() => props.router ? props.router.push("/about") : console.log("router not defined") }>Click Me</button>
+    <div/> */}
       <div className="mt-4">
-        <RecentArticles/>
+        <RecentArticles />
       </div>
-    </Layout>}
+    </Layout>
+  }
+  else {
+    throw new Error("Router not defined. Something's up with Next.js");
+  }
+}
 )
